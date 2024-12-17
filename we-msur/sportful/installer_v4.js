796eaf2f6406c6b7401b9b23304a1ae8bb75d779
Christian Stamati <91951421+cstamati@users.noreply.github.com>
2024-12-17 12:58:48 +0100

    Create installer_v4.js

---
+++ we-msur/sportful/installer_v4.js
@@ -0,0 +1,101 @@
+"use strict";
+var _a, _b;
+const WW_APP_BASE_URL = "https://wewear-size-suggestion.vercel.app";
+const WW_WEB_COMPONENTS = "https://unpkg.com/@wewear/web-components@1.0.35/dist/wewear-web-components.umd.js";
+const WW_LANG = (_b = (_a = document.querySelector('html')) === null || _a === void 0 ? void 0 : _a.lang) !== null && _b !== void 0 ? _b : "en";
+function addStylesheetLink(url) {
+    const link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.head.appendChild(link);
+}
+async function appendHeadScriptAsync(src, type = "module") {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement("script");
+        script.src = src;
+        script.type = type;
+        script.onload = () => {
+            resolve(script);
+        };
+        script.onerror = () => {
+            reject(new Error(`Failed to load script: ${src}`));
+        };
+        document.head.appendChild(script);
+    });
+}
+window.WeWearInstaller = () => ({
+    getProductName: () => {
+        const header = document.querySelector('#colRight h1.nome');
+        if (!header) {
+            console.warn("Product title not found");
+            return "NOT_FOUND_TITLE";
+        }
+        return header.innerText;
+    },
+    getProductSku: () => {
+        var _a, _b;
+        const header = document.querySelector('#colRight h1.nome');
+        if (!header) {
+            console.warn("Product sku not found");
+            return "NOT_FOUND_TITLE";
+        }
+        const sku = (_b = (_a = header.dataset.code) === null || _a === void 0 ? void 0 : _a.split('_').shift()) === null || _b === void 0 ? void 0 : _b.trim();
+        return sku !== null && sku !== void 0 ? sku : "NOT_FOUND_SKU";
+    },
+    getProductImg: () => {
+        const img = document.querySelector('.image img');
+        return img.src;
+    },
+    getWidgetContainer: () => {
+        return document.querySelector("div.page-details-variants-select");
+    },
+    getLanguage: () => {
+        return WW_LANG;
+    },
+    getProductFit: () => {
+        const span = document.querySelector('#colRight span.fitblock-name:nth-of-type(2) span.fitblock-value');
+        return span === null || span === void 0 ? void 0 : span.innerText;
+    },
+    getCaption: () => {
+        const caption = document.querySelector('div.carousel-caption span.small');
+        return caption.innerHTML;
+    }
+});
+async function WeWearInit() {
+    const origin = window.location.origin;
+    if (!origin.includes(".mvcgroup.com")) {
+        return;
+    }
+    console.log("WeWear Init");
+    const installer = window.WeWearInstaller();
+    const container = installer.getWidgetContainer();
+    if (!container) {
+        console.warn("Failed to find widget container");
+        return;
+    }
+    addStylesheetLink(`${WW_APP_BASE_URL}/static/styles/main.css`);
+    await appendHeadScriptAsync(WW_WEB_COMPONENTS, "module");
+    const config = {
+        container,
+        // product info
+        productName: installer.getProductName(),
+        productImageUrl: installer.getProductImg(),
+        sku: installer.getProductSku(),
+        caption: installer.getCaption(),
+        productFit: installer.getProductFit(),
+        // paths
+        appBaseUrl: WW_APP_BASE_URL,
+        mySizePage: `${WW_APP_BASE_URL}/v1/my-size`,
+        findOutSizePage: `${WW_APP_BASE_URL}/v1/user-data`,
+        brandUserId: "",
+        domain: origin,
+        debugMode: false,
+        language: installer.getLanguage(),
+        // for karpos
+        adjustAfterRefine: false
+    };
+    console.log("Loading wewear size flow ", config);
+    WeWearWebComponents.init(config);
+}
+setTimeout(WeWearInit, 4000);
